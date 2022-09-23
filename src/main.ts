39bf63511712626deb79a03752cc49e408e725ba
Mateus Deitos <mate.deitos@hotmail.com>
2022-09-23 07:45:34 -0300

    Removendo cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, { cors: { methods: ["GET"], origin: process.env.FRONTEND_URL } });
+	const app = await NestFactory.create(AppModule);
 	await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
