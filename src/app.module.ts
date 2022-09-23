e31b6dd063ea525bb21a846177097fd35b79f160
Mateus Deitos <mate.deitos@hotmail.com>
2022-09-23 00:19:46 -0300

    Removendo arquivos e setando cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GlobalErrorHandlerFilter } from './global-error-handler.filter';
 import { LaunchesModule } from './launches/launches.module';
 
 @Module({
 	imports: [LaunchesModule, ConfigModule.forRoot({ isGlobal: true })],
-	controllers: [AppController],
 	providers: [
-		AppService,
 		{
 			provide: APP_FILTER,
 			useClass: GlobalErrorHandlerFilter,
